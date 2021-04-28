aabd0344a9fefa70508a99600e39fb0e4fda0ed2
kkkta <kkkta@192.168.1.134>
2021-04-28 11:29:25 +0900

    오류 수정

--- board/src/test/java/board/BoardApplicationTests.java
+++ board/src/test/java/board/BoardApplicationTests.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package board;
 
 import org.junit.jupiter.api.Test;
@@ -22,28 +22,4 @@ class BoardApplicationTests {
 	}
 
 }
-=======
-package board;
 
-import org.junit.jupiter.api.Test;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BoardApplicationTests {
-
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	@Test
-	public void contextLoads() {
-	}
-	
-	@Test
-	public void testSq1session() throws Exception{
-		System.out.println(sqlSession.toString());
-	}
-	
-}
->>>>>>> refs/remotes/origin/testmaster
